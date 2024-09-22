bae10657a0d497f421660f74bcffd0eaa12a1d64
phamthanhnhanussh@gmail.com <phamthanhnhanussh@gmail.com>
2024-09-22 23:14:44 +0700

    first commit

---
+++ Models/Participant.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace QuizApi.Models
+{
+    public class Participant
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int TimeTaken { get; set; }
+    }
+}
---
+++ Models/Question.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuizApi.Models
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string QuestionInWords { get; set; } 
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string? ImageName { get; set; }   
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string Option1 { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string Option2 { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string Option3 { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string Option4 { get; set; }
+    
+        public int Answer { get; set; }
+    }
+}
